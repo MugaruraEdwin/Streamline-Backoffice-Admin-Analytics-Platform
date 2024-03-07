a2818bcae0fa5e5df786f8c30dfe93ae2c991307
MugaruraEdwin <mugarura13@gmail.com>
2024-03-07 15:49:32 +0300

    Adding dynamic port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-const port = 3000;
+const port = process.env.PORT;
 
 mongoose.connect("mongodb+srv://Mugarura:Kristen12$@streamline-analytics.ayhamee.mongodb.net/");
 
